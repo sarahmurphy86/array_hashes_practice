deee33292a48301eee8e80739b2ca247b3193935
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-11 17:54:40 +0100

    Exercise 2 - half questions completed

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,1 +1,89 @@
-#Exeecise 2
+#Exercise B
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
+# Complete these tasks:
+#
+# Get Jonathan's Twitter handle (i.e. the string "jonnyt")
+p users["Jonathan"] [:twitter]
+
+# Get Erik's hometown
+p users["Erik"] [:home_town]
+
+# Get the array of Erik's lottery numbers
+p users ["Erik"] [:lottery_numbers]
+
+# Get the type of Avril's pet Monty
+p users ["Avril"] [:pets][0][:species]
+
+# Get the smallest of Erik's lottery numbers
+result = users ["Erik"] [:lottery_numbers]
+p result.sort[0]
+
+# Return an array of Avril's lottery numbers that are even
+
+result = users ["Avril"] [:lottery_numbers]
+for even in result
+  if (even%2 ==0)
+    p even
+  end
+end
+
+
+# Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+# Change Erik's hometown to Edinburgh
+# Add a pet dog to Erik called "Fluffy"
+# Add another person to the users hash
